6fce36fb74e68e3492df5a61ad49bc0c3f52cf45
谭翮舜 <hepsontam@gmail.com>
2021-06-27 01:29:36 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -51,10 +51,10 @@ mkdir parted
 cp luci-app-diskman/Parted.Makefile parted/Makefile
 
 # Add luci-app-dockerman
-git clone --depth=1 https://github.com/KFERMercer/luci-app-dockerman
-mkdir luci-lib-docker
-curl -s -o ./luci-lib-docker/Makefile https://raw.githubusercontent.com/lisaac/luci-lib-docker/master/Makefile
-rm -rf ../lean/luci-app-docker
+#git clone --depth=1 https://github.com/KFERMercer/luci-app-dockerman
+#mkdir luci-lib-docker
+#curl -s -o ./luci-lib-docker/Makefile https://raw.githubusercontent.com/lisaac/luci-lib-docker/master/Makefile
+#rm -rf ../lean/luci-app-docker
 
 # Add luci-theme-argon
 # git clone --depth=1 https://github.com/jerrykuku/luci-theme-argon
